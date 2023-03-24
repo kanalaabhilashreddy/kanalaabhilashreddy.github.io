00c9f780f03fb088fe5853dedab06c0488453c51
kanalaabhilashreddy <kanalaabhilashreddy@outlook.com>
2023-03-24 20:52:55 +0530

    t1.1

--- scripts/card.js
+++ scripts/card.js
@@ -1,2 +1,12 @@
-const itemCard = document.getElementsByClassName(item-container)
-console.log(itemCard)
+//#region 
+const toggleButton = document.getElementById('order-toggle');
+const orderText = document.querySelector('.card-item-order-text');
+
+toggleButton.addEventListener('click', function() {
+  if (orderText.style.display === 'none') {
+    orderText.style.display = 'flex';
+  } else {
+    orderText.style.display = 'none';
+  }
+});
+//#endregion
